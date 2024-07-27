39a5768237b86a156c39cd4724614744d8dc9703
vinod220103 <161713000+vinod220103@users.noreply.github.com>
2024-07-27 05:14:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Toggle function for burger menu
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('active');
+}
